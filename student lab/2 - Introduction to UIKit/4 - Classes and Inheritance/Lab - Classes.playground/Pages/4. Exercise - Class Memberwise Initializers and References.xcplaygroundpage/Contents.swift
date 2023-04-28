81e1a7cf29d39e8a5c3adab581b6e25db8ec71b5
Jack Lee <dongjunlee222@gmail.com>
2023-04-28 13:15:01 +0900

    class study finished

--- student lab/2 - Introduction to UIKit/4 - Classes and Inheritance/Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
+++ student lab/2 - Introduction to UIKit/4 - Classes and Inheritance/Lab - Classes.playground/Pages/4. Exercise - Class Memberwise Initializers and References.xcplaygroundpage/Contents.swift
@@ -7,6 +7,12 @@ class Spaceship {
     let name: String
     var health: Int
     var position: Int
+    
+    init(name: String, health: Int, position: Int) {
+        self.name = name
+        self.health = health
+        self.position = position
+    }
 
     func moveLeft() {
         position -= 1
@@ -27,6 +33,12 @@ class Spaceship {
 class Fighter: Spaceship {
     let weapon: String
     var remainingFirePower: Int
+    
+    init(weapon: String, remainingFirePower: Int, name: String, health: Int, position: Int) {   /// 세상 불편하네....
+        self.weapon = weapon
+        self.remainingFirePower = remainingFirePower
+        super.init(name: name, health: health, position: position)
+    }
 
     func fire() {
         if remainingFirePower > 0 {
@@ -39,7 +51,13 @@ class Fighter: Spaceship {
 
 class ShieldedShip: Fighter {
     var shieldStrength: Int
-
+    
+    init(shieldStrength: Int, weapon: String, remainingFirePower: Int, name: String, health: Int, position: Int) {  /// 세상 불편하네....
+        self.shieldStrength = shieldStrength
+        super.init(weapon: weapon, remainingFirePower: remainingFirePower, name: name, health: health, position: position)
+    }
+    
+    
     override func wasHit() {
         if shieldStrength > 0 {
             shieldStrength -= 5
@@ -53,13 +71,16 @@ class ShieldedShip: Fighter {
 
  Then create an instance of `Spaceship` below called `falcon`. Use the memberwise initializer you just created. The ship's name should be "Falcon."
  */
-
+let falcon = Spaceship(name: "Falcon", health: 100, position: 0)
+print(falcon.name)
 
 /*:
  Writing initializers for subclasses can get tricky. Your initializer needs to not only set the properties declared on the subclass, but also set all of the uninitialized properties on classes that it inherits from. Go to the declaration of `Fighter` and write an initializer that takes an argument for each property on `Fighter` and for each property on `Spaceship`. Set the properties accordingly. (Hint: you can call through to a superclass's initializer with `super.init` *after* you initialize all of the properties on the subclass).
 
  Then create an instance of `Fighter` below called `destroyer`. Use the memberwise initializer you just created. The ship's name should be "Destroyer."
  */
+let destroyer = Fighter(weapon: "Laser", remainingFirePower: 10, name: "Destroyer", health: 100, position: 0)
+print(destroyer.name)
 
 
 /*:
@@ -67,11 +88,19 @@ class ShieldedShip: Fighter {
 
  Then create an instance of `ShieldedShip` below called `defender`. Use the memberwise initializer you just created. The ship's name should be "Defender."
  */
-
-
+let defender = ShieldedShip(shieldStrength: 100, weapon: "cannon", remainingFirePower: 10, name: "Defender", health: 100, position: 0)
+print(defender.name)
 //:  Create a new constant named `sameShip` and set it equal to `falcon`. Print out the position of `sameShip` and `falcon`, then call `moveLeft()` on `sameShip` and print out the position of `sameShip` and `falcon` again. Did both positions change? Why? If both were structs instead of classes, would it be the same? Why or why not? Provide your answer in a comment or print statement below.
+let sameShip = falcon
+print(sameShip.position)
+print(falcon.position)
 
+sameShip.moveLeft()
+falcon.moveRight()
+print(sameShip.position)
+print(falcon.position)
 
+/// class shares the same memory space. Therefore, instances with the copy of an instance would mean - they share the same memory. unlike structure where individual instance exists as it is value reference
 /*:
  _Copyright © 2021 Apple Inc._
 
