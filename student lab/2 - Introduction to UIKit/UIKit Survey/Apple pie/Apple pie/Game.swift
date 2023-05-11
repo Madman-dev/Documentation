521c99a9b48ba45abd8d061d2bc074001fc8d68c
Jack Lee <dongjunlee222@gmail.com>
2023-05-11 14:11:02 +0900

    Apple pie project finished

--- student lab/2 - Introduction to UIKit/UIKit Survey/Apple pie/Apple pie/Game.swift
+++ student lab/2 - Introduction to UIKit/UIKit Survey/Apple pie/Apple pie/Game.swift
@@ -10,26 +10,16 @@ import Foundation
 struct Game {
     var word: String
     var numberOfTurns: Int
-    var guessedLetters: [Character]
-//    var formattedWords: String {
-//        var emptyWord: String = ""
-//        for letter in word {
-//            if guessedLetters.contains(letter) {
-//                emptyWord += "\(letter)"
-//            } else {
-//                emptyWord.append(letter)
-//            }
-//        }
-//        return emptyWord
-//    }
-    
-    var formattedWord: String {
-        var guessedWord = ""
+    var guessedLetters: [Character] // 내가 예측한 알파벳 모음 - 내가 누른 알파벳
+    var formattedWord: String {     // 화면에 _ 로 단어를 표시해줄 수 있도록 만든 요소?
+        var guessedWord = ""        // 이건 여기 왜 있는거지? 🙋🏻‍♂️
+        /// 애초에 여기서 출력되는 값을 Character 타입으로 선언하게 되는 거였구나 - String 타입에서 뽑아 쓰는걸로 이해되니까
         for letter in word {
             if guessedLetters.contains(letter) {
-                guessedWord += "\(letter)"  /// 이건 왜 이렇게 해야하지? >> 이게 String으로 변환을 해주는건가?
+                guessedWord += "\(letter)"  /// 이건 왜 이렇게 해야하지? >> 이게 String으로 변환을 해주는건가? >> 변환해서 넣어주네
             } else {
                 guessedWord += "_"  /// 🙋🏻‍♂️ append랑 어떤 차이점이 있는거지?
+                /// " _ "를 할 수도 있지만, 더 좋은 방법이 있다. + 이 문제는 인터페이스를 더 좋게 발달 시키는 것이지 데이터 값을 가지고 장난을 치는게 아니다
             }
         }
         return guessedWord
--- student lab/2 - Introduction to UIKit/UIKit Survey/Apple pie/Apple pie/ViewController.swift
+++ student lab/2 - Introduction to UIKit/UIKit Survey/Apple pie/Apple pie/ViewController.swift
@@ -11,8 +11,18 @@ class ViewController: UIViewController {
     
     var listOfWords = ["buccaneer", "swift", "naver", "program", "coding"]
     let incorrectMovesAllowed = 7
-    var totalWins = 0
-    var totalLosses = 0
+    /// wins이 바뀌면 newRound()를 시작하도록?
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     /// !을 사용해도 되나?
     var currentGame: Game!
 
@@ -28,24 +38,69 @@ class ViewController: UIViewController {
     }
     
     func newRound() {
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, numberOfTurns: incorrectMovesAllowed, guessedLetters: [])     /// 아예 새롭게 받아야 하니까 guessedLetter는 [] 비어두는 것
-        updateUI()
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, numberOfTurns: incorrectMovesAllowed, guessedLetters: [])     /// 아예 새롭게 받아야 하니까 guessedLetter는 [] 비어두는 것
+            updateUI()
+            enableAllButtons(true)
+        } else {
+            /// 아래가 왜 false이지? >>> 새로운 라운드를 시작하는데 - button을 비활성화 할 수 없지~ 새로운 라운드를 시작할 때 값이 없다면 button을 꺼라~ 이런 구성
+            enableAllButtons(false)
+        }
     }
     
+    /// 나무 이미지 변경 + 횟수 변경
     func updateUI() {
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
         treeImageView.image = UIImage(named: "Tree \(currentGame.numberOfTurns)")   // 오... 이렇게도 이미지 즉각 변경이 가능해?
-        correctWordLabel.text = currentGame.formattedWord   // 여기서 접근하는게 이렇게 되는거구나
+        
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))  /// letter은 여전히 Character
+        }
+        let spacedWords = letters.joined(separator: " ")
+        correctWordLabel.text = spacedWords
+        
+        
+        /// 생각했던 것보다 모델 데이터에서 직접적인 변화를 주는 건 없구나
+        //        correctWordLabel.text = currentGame.formattedWord   // 여기서 접근하는게 이렇게 되는거구나
+//        let letters: [String]   /// 이 방식은 안되네 - : [String] 타입으로 지정을 하게 되면 initialize를 해야하니까
+//        for word in currentGame.formattedWord {
+//            letters.append(String(word))
+//        }
     }
 
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
-//        listOfWords.first?.contains(sender!)
+///        listOfWords.first?.contains(sender!) >> 데이터 값을 뽑아쓰는건 Game 파일에서 실행
         let letterString = sender.title(for: .normal)!
+        /// 전달 받은 알파벳을 모두 소문자
         let letter = Character(letterString.lowercased())   // 오... 신기한데
         currentGame.playerGuessed(letter: letter)
-        updateUI()
+//        updateUI()
+        updateGameState()
+    }
+    
+    /// 🙋🏻‍♂️ 계속 업데이트가 win에만 발생하길래 무슨 문제가 있는건가 했는데 - 현재 게임 상황으로 확인을 한게 아니였구나 currentGame.numberOfTurns, NOT incorrectMovesAllowed
+    func updateGameState() {
+        if currentGame.numberOfTurns == 0 {
+            totalLosses += 1
+//            updateUI()
+            //newRound() >> 여기에서 newRound를 호출하는게 아니네
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+//            updateUI()
+            //newRound() >> 여기에서 newRound를 호출하는게 아니네
+        } else {
+            updateUI()
+        }
+    }
+
+    /// 버튼 값 눌릴 수 있게 선언 >>> 이건 몰랐네.. 🙋🏻‍♂️
+    /// True면 enable을 하는거고 - 아니면 false를 돌리는거네
+    func enableAllButtons(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = true
+        }
     }
 }
-
