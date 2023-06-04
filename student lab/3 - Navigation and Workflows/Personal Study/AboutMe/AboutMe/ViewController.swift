02f5e28734df32b4bf6ca93a1cd89c98d49ed112
Jack Lee <dongjunlee222@gmail.com>
2023-06-04 23:22:05 +0900

    personal info app

---
+++ student lab/3 - Navigation and Workflows/Personal Study/AboutMe/AboutMe/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Jack Lee on 2023/06/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
