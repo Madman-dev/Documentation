7b96e6693ddecca11f0cc583cdbb8805334bc76d
Jack Lee <dongjunlee222@gmail.com>
2023-06-10 23:50:14 +0900

    navigation and workflows - 6 fundamental characteristics to keep in mind when designing app

---
+++ student lab/3 - Navigation and Workflows/Personal Study/Additional Documentation record.playground/Pages/Building Simple Workflows.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+//: [Previous](@previous)
+
+import Foundation
+
+/*
+ - how to break down a feature into an intuitive workflow
+ - how to design a navigation hierarchy for an app
+ - where to learn more about interface conventions
+ 
+ 
+ to build Googood apps, I have to become comfortable and understand when and how to use UI Objects in a familiar way
+ The fundamental 6 characteristics that I need to keep in mind is...
+ 
+ 1. Aesthetic integrity
+ the appearance and behavior should make sense for its goal and its purpose. an app that helps people perfom serious task can keep them focused by using subtle graphics, standard controls and predictable interactions. entertaining apps such as games can sport a captivating appearance that promises fun and excitement - while encouraging discovery
+ 
+ 2. consistency
+ use well known icons, standard text styles, and uniform terminology to enable people to expect what to do next.
+ 
+ if you're designing a workflow for completing an order in an e-commerce app - take a look at the checkout flow of popular e-commerce apps. you shouldn't be copying other apps for no reason. But you can use a good example as a starting point for your design.
+ 
+ 3. direct manipulation
+ through direct manipulation users can see immediate, visible result of their action.
+ 
+ 4. feedback
+ users should never wonder if an app responded to their actions. make sure the app provides a noticeable feedback. alers animations etc
+ 
+ 5. metaphors
+ apps object and actions are metaphors that allows users to catch up on what's going to happen. Computers in home screen is called desktop as during the early version of GI, they were intended to help users feel at home during the new computing paradigm.
+ 
+ 6. user control
+ Before apps, there are the users. The users should always make the important decision. You have to give users decision making powers without bombarding them with alerts and options at every juncture.
+ */
+
+
+//what are workflows?
+
+
+
+
+
+//: [Next](@next)
